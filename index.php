c24fa8f938b85d14e03aa0a5b2c13295e903ce7b
YIZUZ19 <fdggr469@gmail.com>
2021-11-08 18:30:00 -0600

    index actualizado

--- index.php
+++ index.php
@@ -7,8 +7,8 @@ echo "El nombre del usuario es: ".$objUsario1->strNombre."<br>";
 echo "Su correo es: ".$objUsario1->strEmail."<br>";
 echo "Su edad es: ".$objUsario1->intEdad."<br>";
 echo "El tido de usuario es: ".$objUsario1->strTipo."<br>";
-#echo "La clave es: ".$objUsario1->strClave."<br>";
-#echo "La fecha de creacion es: ".$objUsario1->strFecha."<br>";
+echo "La clave es: ".$objUsario1->strClave."<br>";
+echo "La fecha de creacion es: ".$objUsario1->strFecha."<br>";
 echo "<hr>";
 
 $objUsario2 = new Usuario("Manuel Monzon","mm8a@gmail.com",22,"Cliente");
@@ -17,7 +17,7 @@ echo "El nombre del usuario es: ".$objUsario2->strNombre."<br>";
 echo "Su correo es: ".$objUsario2->strEmail."<br>";
 echo "Su edad es: ".$objUsario2->intEdad."<br>";
 echo "El tido de usuario es: ".$objUsario2->strTipo."<br>";
-#echo "La clave es: ".$objUsario2->strClave."<br>";
-#echo "La fecha de creacion es: ".$objUsario2->strFecha."<br>";
+echo "La clave es: ".$objUsario2->strClave."<br>";
+echo "La fecha de creacion es: ".$objUsario2->strFecha."<br>";
 echo "<hr>";
 ?>
